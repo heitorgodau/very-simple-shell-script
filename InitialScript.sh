6a2f644727df835527e954616ce1b2b8cfe1454e
Heitor Godau <40405944+heitorgodau@users.noreply.github.com>
2019-12-27 00:50:17 -0300

    V0.2
    Improve git config variables

--- InitialScript.sh
+++ InitialScript.sh
@@ -44,8 +44,12 @@ flatpak install flathub com.meetfranz.Franz
 
 sudo apt-get install terminator
 sudo apt-get install git
-git config --global user.name "Heitor Godau"
-git config --global user.email heitorgodau@gmail.com
+echo "What name you want in your user.name Git config?"
+read -p 'user.name: ' varname
+git config --global user.name "$varname"
+echo "What name you want in your user.email Git config?"
+read -p 'user.email: ' varemail
+git config --global user.email $varemail
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
